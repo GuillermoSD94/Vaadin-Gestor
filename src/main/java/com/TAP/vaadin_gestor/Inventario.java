e77a3cd2f1db8e04913968b65ac9a76518292007
Guillermo <guillermosd94@hotmail.es>
2019-01-03 21:36:03 +0100

    Gestor 0.2

---
+++ src/main/java/com/TAP/vaadin_gestor/Inventario.java
@@ -0,0 +1,39 @@
+package com.TAP.vaadin_gestor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario {
+	
+	private static Inventario singleton;
+	private List<Producto> productos;
+
+	private Inventario() {
+		super();
+		productos = new ArrayList<>();
+	}
+	
+	public static Inventario getInstance() {
+		
+		if(singleton == null)
+			singleton = new Inventario();
+		
+		return singleton;
+	}
+	
+	public void addProducto(Producto p) {
+		productos.add(p);
+	}
+	
+	public void deleteProducto(Producto p)
+	{
+		productos.remove(p);
+	}
+	
+	public List<Producto> getProductos() {
+		return productos;
+	}
+	
+
+
+}
--- src/main/java/com/TAP/vaadin_gestor/MyUI.java
+++ src/main/java/com/TAP/vaadin_gestor/MyUI.java
@@ -7,10 +7,16 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Grid.SelectionMode;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -22,7 +28,7 @@ import com.vaadin.ui.VerticalLayout;
 @Theme("mytheme")
 public class MyUI extends UI {
 
-    @Override
+    /*@Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
         
@@ -38,8 +44,162 @@ public class MyUI extends UI {
         layout.addComponents(name, button);
         
         setContent(layout);
-    }
+    }*/
 
+private Producto productoSeleccionado; 
+	
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+       
+    	Grid<Producto> grid = new Grid<Producto>();
+    	
+    	HorizontalLayout horizontalLayout = new HorizontalLayout();
+    	
+    	/* VENTANA DETALLE */
+    	
+    	Window subWindow = new Window("Detalles del producto");
+        VerticalLayout subContent1 = new VerticalLayout();
+        VerticalLayout subContent2 = new VerticalLayout();
+        VerticalLayout subContent3 = new VerticalLayout();
+        HorizontalLayout horizontalLayoutWindow = new HorizontalLayout();
+        
+        Label labelNombre = new Label();
+        Label labelCantidad = new Label();
+        Label labelPrecio = new Label();
+        TextField textFieldNombreVentana = new TextField("Nombre");
+    	TextField textFieldCantidadVentana = new TextField("Cantidad");
+    	TextField textFieldPrecioVentana = new TextField("Precio");
+        Button buttonDelete = new Button("Eliminar producto");
+        Button buttonModifyNombre = new Button("Modificar nombre");
+        Button buttonModifyCantidad = new Button("Modificar cantidad");
+        Button buttonModifyPrecio = new Button("Modificar precio");
+        
+        buttonDelete.addClickListener(e -> {
+        	Inventario.getInstance().deleteProducto(productoSeleccionado);
+        	grid.setItems(Inventario.getInstance().getProductos());
+        	removeWindow(subWindow);
+        });
+    	
+        buttonModifyNombre.addClickListener(e -> {
+        	Producto p = new Producto(textFieldNombreVentana.getValue());
+    		productoSeleccionado.setNombre(p.getNombre());
+    		grid.setItems(Inventario.getInstance().getProductos());
+    		textFieldNombreVentana.clear();
+    		removeWindow(subWindow);
+        });
+        
+        buttonModifyCantidad.addClickListener(e -> {
+        	Producto p = new Producto(textFieldCantidadVentana.getValue());
+    		productoSeleccionado.setCantidad(p.getNombre());
+    		grid.setItems(Inventario.getInstance().getProductos());
+    		textFieldCantidadVentana.clear();
+    		removeWindow(subWindow);
+        });
+        
+        buttonModifyPrecio.addClickListener(e -> {
+        	Producto p = new Producto(textFieldPrecioVentana.getValue());
+    		productoSeleccionado.setPrecio(p.getNombre());
+    		grid.setItems(Inventario.getInstance().getProductos());
+    		textFieldPrecioVentana.clear();
+    		removeWindow(subWindow);
+        });
+        
+      
+        subContent1.addComponents(new Label("Nombre:"), labelNombre, new Label("Cantidad:"),
+        		labelCantidad, new Label("Precio:"), labelPrecio, buttonDelete);
+        
+        subContent2.addComponents(textFieldNombreVentana, textFieldCantidadVentana, textFieldPrecioVentana);
+        
+        subContent3.addComponents(buttonModifyNombre, new Label(), buttonModifyCantidad,
+        		new Label(), buttonModifyPrecio);
+        
+        horizontalLayoutWindow.addComponents(subContent1, subContent2, subContent3);
+        
+        
+        
+        subWindow.center();
+        subWindow.setContent(horizontalLayoutWindow);
+        
+        
+        
+        // addWindow(subWindow);
+    	
+    	/* TABLE */
+    	
+    	
+    	grid.addColumn(Producto::getNombre).setCaption("Nombre");
+    	grid.addColumn(Producto::getCantidad).setCaption("Cantidad");
+    	grid.addColumn(Producto::getPrecio).setCaption("Precio");
+    	grid.setSelectionMode(SelectionMode.SINGLE);
+    	
+    	grid.addItemClickListener(event -> {
+    		
+    		productoSeleccionado = event.getItem();
+    		
+        	// Notification.show("Value: " + event.getItem());
+        	labelNombre.setValue(productoSeleccionado.getNombre());
+        	labelCantidad.setValue(productoSeleccionado.getCantidad());
+        	labelPrecio.setValue(productoSeleccionado.getPrecio());
+        	
+        	
+        	removeWindow(subWindow);
+        	addWindow(subWindow);
+        	
+    	});
+    	
+    	
+    	/* FORM */
+    	
+    	
+    	FormLayout formLayout = new FormLayout();
+    	
+    	TextField textFieldNombre = new TextField("Nombre");
+    	TextField textFieldCantidad = new TextField("Cantidad");
+    	TextField textFieldPrecio = new TextField("Precio");
+    	Button buttonAdd = new Button("Añadir nuevo producto");
+    			
+    	buttonAdd.addClickListener(e -> {
+    		
+    		Producto p = new Producto(
+    				textFieldNombre.getValue(),
+    				textFieldCantidad.getValue(),
+    				textFieldPrecio.getValue()
+    				);
+    		
+    		Inventario.getInstance().addProducto(p);
+    		
+    		textFieldNombre.clear();
+    		textFieldCantidad.clear();
+    		textFieldPrecio.clear();
+    		
+    		grid.setItems(Inventario.getInstance().getProductos());
+    		
+    		
+    		Notification.show("¡Producto añadido! Ya tenemos " + 
+    				Inventario.getInstance().getProductos().size(),
+    				Notification.TYPE_TRAY_NOTIFICATION);
+    		
+    	});
+    	
+    	
+    	
+    	formLayout.addComponents(
+    			textFieldNombre, 
+    			textFieldCantidad, 
+    			textFieldPrecio, 
+    			buttonAdd
+    	);
+    	
+    
+    	horizontalLayout.addComponents(grid, formLayout);
+    	
+    	
+    	
+    	setContent(horizontalLayout);
+    	
+    	
+    }
+	
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
